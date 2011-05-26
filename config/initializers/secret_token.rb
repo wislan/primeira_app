36136cbde740a6e4470806bb036a56c4632e272b
wislan <wislan_lima@hotmail.com>
2011-05-25 22:01:26 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeiraApp::Application.config.secret_token = '9da41bdac3aad8193beefbf8ba88440638fa63013ef0cd84eee310b7d478033bd0135954634ffa6f59e07a5f2807019a01d9375b4062d92e95542da8ab597ee8'
